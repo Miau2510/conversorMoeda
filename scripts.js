323983603c2526c3268b4df0c4460be391153747
= <=>
2026-08-20 15:54:56 -0300

    v1.5

--- scripts.js
+++ scripts.js
@@ -1,24 +1,32 @@
+const moedas = {
+    "USD": 5.19,
+    "EUR": 6.06,
+    "GBP": 7.07,
+    "JPY": 0.03,
+    "ARS": 0.003,
+    "CNY": 0.72,
+    "CAD": 3.76,
+    "AUD": 3.42
+}
+
+let reais, moeda, moedaEstrangeira = 0.00;
+
+const input = document.getElementById("real");
+const select = document.getElementById("moedaEs");
+
+
 function converte() {
+    reais = document.getElementById("real").value;
+    moeda = document.getElementById("moedaEs").value;
 
-    const moedas = {
-        "USD": 5.19,
-        "EUR": 6.06,
-        "GBP": 7.07,
-        "JPY": 0.03,
-        "ARS": 0.003,
-        "CNY": 0.72,
-        "CAD": 3.76,
-        "AUD": 3.42
-    }
-
-    let reais = document.getElementById("real").value;
-    let moeda = document.getElementById("moedaEs").value;
-
-    let moedaEstrangeira = 0.00;
+    moedaEstrangeira = 0.00;
 
     if (moeda in moedas) {
         moedaEstrangeira = (reais / moedas[moeda]).toFixed(2);
     }
 
-    document.getElementById("moedaE").value = moedaEstrangeira;
+    document.getElementById("moedaE").value = moedaEstrangeira
 }
+input.addEventListener("input", converte)
+
+select.addEventListener("change", converte)
