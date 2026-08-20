0b62d339cbc8571820902c06c2438e391c40d348
= <=>
2026-08-20 14:34:47 -0300

    v1.2

--- scripts.js
+++ scripts.js
@@ -1,7 +1,24 @@
 function converte() {
-    reais = document.getElementById("real").value;
 
-    moedaEstrangeira = (reais / 5.19).toFixed(2);
+    const moedas = {
+        "USD": 5.19,
+        "EUR": 6.06,
+        "GBP": 7.07,
+        "JPY": 0.03,
+        "ARS": 0.003,
+        "CNY": 0.72,
+        "CAD": 3.76,
+        "AUD": 3.42
+    }
+
+    let reais = document.getElementById("real").value;
+    let moeda = document.getElementById("moedaEs").value;
+
+    let moedaEstrangeira = 0.00;
+
+    if (moeda in moedas) {
+        moedaEstrangeira = (reais / moedas[moeda]).toFixed(2);
+    }
 
     document.getElementById("moedaE").value = moedaEstrangeira;
 }
